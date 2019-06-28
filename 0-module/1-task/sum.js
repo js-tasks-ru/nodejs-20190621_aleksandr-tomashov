4ec6f710e8a0a8e10c31b5ee9dead1c60b7c15ce
Alexander Tomashov <Sanya-81-chess@mail.ru>
2019-06-29 01:21:29 +0800

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,11 +1,11 @@
 function sum(a, b) {
 
-    if(Number.isFinite(a) && Number.isFinite(b)){
-      return a + b
-    }
-        
-    throw new TypeError(`isn't correct arguments`);
+  if(Number.isFinite(a) && Number.isFinite(b)){
+    return a + b
+  }
   
+  throw new TypeError(`is't `);
+
 }
 
 module.exports = sum;
