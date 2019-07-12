34145851ef736868cbb62fea285049d082b69bad
Alexander Tomashov <Sanya-81-chess@mail.ru>
2019-07-12 16:44:37 +0800

    category and product

--- 6-module/1-task/models/Category.js
+++ 6-module/1-task/models/Category.js
@@ -2,10 +2,29 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: { type: String,  required: true }
 });
 
 const categorySchema = new mongoose.Schema({
-  subcategories: [subCategorySchema]
+  title: { type: String,  required: true },
+  subcategories: [ subCategorySchema ]
 });
+
+const Category = mongoose.model('Category', categorySchema);
+const category = new Category({ 
+  title : "Детские товары и игрушки", 
+  subcategories: [ 
+    {title:'Прогулки и детская комната'},
+    {title:'Кормление и гигиена'},
+    {title:'Игрушки и развлечения'},
+    {title:'Активный отдых и улица'},
+    {title:'Радиоуправляемые модели'},
+    {title:'Школьные товары'}
+  ]    
+});
+
+
+
+console.log(category)
 
 module.exports = connection.model('Category', categorySchema);
--- 6-module/1-task/models/Product.js
+++ 6-module/1-task/models/Product.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category:{ type: mongoose.Types.ObjectId , ref: 'Category', required: true },
+  subcategory: { type: mongoose.Types.ObjectId, required: true },
+  images: [ String ]
 });
+
+const Product = mongoose.model('Product', productSchema);
+const product = new Product({
+  images: ['...', '...'],
+  title: 'Коляска Adamex Barletta 2 in 1',
+  description: 'Универсальная модель, которая с легкостью заменит родителям сразу ...',
+  price: 21230,
+  category: '',
+  subcategory:''
+})
 
 module.exports = connection.model('Product', productSchema);
